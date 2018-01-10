7c3a808318395530718a38e5354642cf641d995a
My Name <kensei416@gmail.com>
2018-01-10 16:37:17 +0900

    Fix create tasks #1

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 20180110021138) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id", "created_at"], name: "index_tasks_on_category_id_and_created_at"
     t.index ["category_id"], name: "index_tasks_on_category_id"
-    t.index [nil, "created_at"], name: "index_tasks_on_user_id_and_created_at"
   end
 
   create_table "users", force: :cascade do |t|
