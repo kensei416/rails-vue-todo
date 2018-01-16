7c31825df790c31dae0c36d7b062e6719fbc0228
My Name <kensei416@gmail.com>
2018-01-16 20:01:29 +0900

    Add Invalid email & password test #7

--- frontend/test/nightwatch/specs/login-spec.js
+++ frontend/test/nightwatch/specs/login-spec.js
@@ -11,13 +11,27 @@ module.exports = {
   
   'Input Invalid Value' : function (browser) {
     browser
-      .url('http://localhost:5000/login') .pause(1000)
-      .setValue('.email input[type=email]', 'nightwatch')
-      .assert.containsText('.email .input-group__error', 'Your Email is Invalid')
-      .setValue('.password input[type=password]', '')
+      .url('http://localhost:5000/login').pause(1000)
+      .setValue('.email .input-group__input input[type=text]', 'nightwatch')
+      .assert.containsText('.email .input-group__error', 'The email field must be a valid email.')
+      .setValue('.password .input-group__input input[type=password]', 'foobar')
+      .assert.elementPresent('.password .input-group__error')
       .click('.checkbox')
-      .click('.login').pause(2000)
-      .assert.containsText('.error-message', 'alert error-message error')
-      
+      .click('.login')
+      .click('.reset')
+      .assert.value('.email .input-group__input input[type=text]', '')
+      .assert.value('.password .input-group__input input[type=password]', '')
+      .end()
+  },
+
+  'Input Invalid Email & Password' : function (browser) {
+    browser
+      .url('http://localhost:5000/login').pause(1000)
+      .assert.cssProperty('.error-message', 'display', 'none')
+      .setValue('.email .input-group__input input[type=text]', 'foobar@gmail.com')
+      .setValue('.password .input-group__input input[type=password]', 'foobar')
+      .click('.checkbox')
+      .click('.login')
+      .assert.cssProperty('.error-message', 'display', 'flex')
   }
 }
