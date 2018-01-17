db7a368486ca1db66e7beb12feff597d8228ee76
My Name <kensei416@gmail.com>
2018-01-17 11:49:20 +0900

    Add Check Cookies When check box is set and unset #7

--- frontend/test/nightwatch/specs/login-spec.js
+++ frontend/test/nightwatch/specs/login-spec.js
@@ -44,5 +44,35 @@ module.exports = {
       .click('.login').pause(1000)
       .assert.urlEquals('http://localhost:5000/')
       .end()
+  },
+
+  'When check box is set Check Cookies' : function (browser) {
+    browser
+      .url('http://localhost:5000/login').pause(1000)
+      .setValue('.email .input-group__input input[type=text]', 'kensei416@gmail.com')
+      .setValue('.password .input-group__input input[type=password]', 'foobar')
+      .click('.checkbox')
+      .click('.login').pause(1000)
+      .getCookies(function (result) {
+        this.assert.equal(result.value.length, 3)
+        this.assert.equal(result.value[0].name, '_todo_sample_session')
+        this.assert.equal(result.value[1].name, 'remember_token')
+        this.assert.equal(result.value[2].name, 'user_id')
+      })
+      .end
+  },
+
+  'When check box is unset value ' : function (browser) {
+    browser
+    .deleteCookies()    
+    .url('http://localhost:5000/login').pause(1000)
+    .setValue('.email .input-group__input input[type=text]', 'kensei416@gmail.com')
+    .setValue('.password .input-group__input input[type=password]', 'foobar')
+    .click('.login').pause(1000)
+    .getCookies(function (result) {
+      this.assert.equal(result.value.length, 1)
+      this.assert.equal(result.value[0].name, '_todo_sample_session')
+    })
+    .end
   }
 }
